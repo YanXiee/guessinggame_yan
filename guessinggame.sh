93615b375a64beb4fd1008291eb85db1bfea4674
yanxiee <xieyan@msu.edu>
2020-01-25 15:09:16 -0500

    revision

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,21 +2,26 @@
 # File: guessinggame.sh
 function guessinggame {
 	n=$(ls -1 | wc -l)
-	echo "Guess how many files are in the current directory?"
 
+	ans=0
+	echo "Please guess how many files in the current directory:"
 
-	for i in {1..100}
+	while [[ $ans -ne $n ]]
 	do
 		read ans
-		if [[ $ans -eq $n ]]
-		then 
-			echo "Congratulation! That's correct!"
-			break
-		elif [[ $ans -lt $n ]]
-		then 
+
+		if [[ $ans -lt $n ]]
+		then
 			echo "$ans is too small, try again:"
-		else 
+		elif [[ $ans -gt $n ]]
+		then
 			echo "$ans is too large, try again:"
-	fi
+		else 
+			echo "That's correct! Well done!"
+		fi
 	done
 }
+
+guessinggame
+
+
